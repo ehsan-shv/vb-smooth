733356cd4dcd7da64e78786d97aedc55a449e32e
eh. sh <sh.ehsan74.es@gmail.com>
2022-02-22 16:39:36 +0330

    init

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import VbSmooth from '@/vb-smooth.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    VbSmooth
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vb-smooth />
+  </div>
+</template>
